ebf6fe3ec9d055b95514f7c0a5432695d7f3ac72
Kirill Darenskii <kdarenskii@mail.ru>
2024-03-07 22:09:54 +0300

    made base vkui setup and added paths plugin to vite config

---
+++ src/components/App.tsx
@@ -0,0 +1,38 @@
+import {
+  AppRoot,
+  Group,
+  Header,
+  Panel,
+  PanelHeader,
+  SimpleCell,
+  SplitCol,
+  SplitLayout,
+  View,
+  usePlatform,
+} from '@vkontakte/vkui';
+
+import '@vkontakte/vkui/dist/vkui.css';
+
+export const App = () => {
+  const platform = usePlatform();
+
+  return (
+    <AppRoot>
+      <SplitLayout
+        header={platform !== 'vkcom' && <PanelHeader delimiter="none" />}
+      >
+        <SplitCol autoSpaced>
+          <View activePanel="main">
+            <Panel id="main">
+              <PanelHeader>VKUI</PanelHeader>
+              <Group header={<Header mode="secondary">Items</Header>}>
+                <SimpleCell>Hello</SimpleCell>
+                <SimpleCell>World</SimpleCell>
+              </Group>
+            </Panel>
+          </View>
+        </SplitCol>
+      </SplitLayout>
+    </AppRoot>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+
+import { AdaptivityProvider, ConfigProvider } from '@vkontakte/vkui';
+import ReactDOM from 'react-dom/client';
+
+import { App } from 'components/App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ConfigProvider>
+      <AdaptivityProvider>
+        <App />
+      </AdaptivityProvider>
+    </ConfigProvider>
+  </React.StrictMode>
+);
