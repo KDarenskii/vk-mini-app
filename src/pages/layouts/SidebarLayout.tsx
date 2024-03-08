13f4d849e3bc1dd2796a1ede23121520f3df18c2
Kirill Darenskii <kdarenskii@mail.ru>
2024-03-08 12:24:34 +0300

    created layout with sidebar navigation

---
+++ src/pages/layouts/SidebarLayout.tsx
@@ -0,0 +1,37 @@
+import { FC, PropsWithChildren } from 'react';
+
+import { NavSidebar } from 'widgets';
+
+import {
+  PanelHeader,
+  SplitCol,
+  SplitLayout,
+  useAdaptivityConditionalRender,
+  usePlatform,
+} from '@vkontakte/vkui';
+
+export const SidebarLayout: FC<PropsWithChildren> = ({ children }) => {
+  const platform = usePlatform();
+  const { viewWidth } = useAdaptivityConditionalRender();
+
+  return (
+    <SplitLayout
+      style={{ justifyContent: 'center' }}
+      header={platform !== 'vkcom' && <PanelHeader delimiter="none" />}
+    >
+      {viewWidth.tabletPlus && (
+        <SplitCol
+          className={viewWidth.tabletPlus.className}
+          fixed
+          width={300}
+          maxWidth={300}
+        >
+          <NavSidebar />
+        </SplitCol>
+      )}
+      <SplitCol width="100%" maxWidth="600px" stretchedOnMobile autoSpaced>
+        {children}
+      </SplitCol>
+    </SplitLayout>
+  );
+};
