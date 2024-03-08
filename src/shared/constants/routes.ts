08f22eaabb283b973f6383830055c3c385755aeb
Kirill Darenskii <kdarenskii@mail.ru>
2024-03-08 12:21:16 +0300

    added constant for routes

---
+++ src/shared/constants/routes.ts
@@ -0,0 +1,14 @@
+export const routes = {
+  root: {
+    path: '/',
+    name: 'home',
+  },
+  fact: {
+    path: '/fact',
+    name: 'fact',
+  },
+  age: {
+    path: '/age',
+    name: 'age',
+  },
+} as const;
