82ba5dfa212b22abfafccf755c535d3a65326a9a
Kirill Darenskii <kdarenskii@mail.ru>
2024-03-09 11:07:28 +0300

    added model to fetch fact

---
+++ src/widgets/factPanel/model/index.ts
@@ -0,0 +1,1 @@
+export { useFactPanel } from './useFactPanel';
---
+++ src/widgets/factPanel/model/useFactPanel.ts
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+import { useQuery } from '@tanstack/react-query';
+
+import { FactService } from 'entities';
+
+export const useFactPanel = () => {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const { refetch, isFetching, isError } = useQuery({
+    queryKey: ['fact'],
+    queryFn: FactService.get,
+    select: (data) => data.data,
+    enabled: isSubmitted,
+  });
+
+  const getFact = async () => {
+    setIsSubmitted(true);
+    const fetchedFact = await refetch();
+    return fetchedFact.data;
+  };
+
+  return { getFact, isFetching, isError };
+};
