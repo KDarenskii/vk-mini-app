15fa0151d4117ae06ad4341a4c8068a18086dfc3
Kirill Darenskii <kdarenskii@mail.ru>
2024-03-10 12:40:17 +0300

    chaged router to hash router

--- src/app/config/router/routerConfig.tsx
+++ src/app/config/router/routerConfig.tsx
@@ -1,11 +1,11 @@
-import { Navigate, createBrowserRouter } from 'react-router-dom';
+import { Navigate, createHashRouter } from 'react-router-dom';
 
 import { AgePage } from 'pages/agePage';
 import { FactPage } from 'pages/factPage';
 
 import { routes } from 'shared/constants';
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: routes.root.path,
     element: <Navigate to={routes.fact.path} />,
