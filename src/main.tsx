ac746336443430f566090185024604c546c0474d
Kirill Darenskii <kdarenskii@mail.ru>
2024-03-10 12:11:12 +0300

    added vk bridge initializer

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import bridge from '@vkontakte/vk-bridge';
 import ReactDOM from 'react-dom/client';
 
 import { App } from 'app/App';
+
+bridge.send('VKWebAppInit');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
