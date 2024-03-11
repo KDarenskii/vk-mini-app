bca448cc6713ff09ae114e09ea19c81d2f637279
Kirill Darenskii <kdarenskii@mail.ru>
2024-03-11 13:12:42 +0300

    setted default theme dark

--- src/app/providers/WithTheme.tsx
+++ src/app/providers/WithTheme.tsx
@@ -2,16 +2,12 @@ import React, { ReactNode, useCallback, useState } from 'react';
 
 import { ThemeContext, ThemeSwitcherContext } from 'shared/context';
 
-import { useAppearance } from '@vkontakte/vkui';
-
 type ThemeProviderProps = {
   children: ReactNode;
 };
 
 export const WithTheme: React.FC<ThemeProviderProps> = ({ children }) => {
-  const appearance = useAppearance();
-
-  const [theme, setTheme] = useState<'light' | 'dark'>(appearance);
+  const [theme, setTheme] = useState<'light' | 'dark'>('dark');
 
   const toggleTheme = useCallback(() => {
     setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
